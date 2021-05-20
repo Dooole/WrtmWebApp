f2cbcaab495b36854915af0cfa30b61bd79b2e24
Dooole <75305901+Dooole@users.noreply.github.com>
2021-05-20 13:36:00 +0300

    Add project files.

---
+++ WrtmWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WrtmWebApp.Startup))]
+namespace WrtmWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
