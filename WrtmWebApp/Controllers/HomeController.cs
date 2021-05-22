43da442b985b6514259bd82976a3042bfc291f61
Dooole <75305901+Dooole@users.noreply.github.com>
2021-05-22 16:47:41 +0300

    Show device list in dashboard

--- WrtmWebApp/Controllers/HomeController.cs
+++ WrtmWebApp/Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WrtmWebApp.Models;
 
 namespace WrtmWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private wrtmDBEntities db = new wrtmDBEntities();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Devices.ToList());
         }
 
         public ActionResult About()
