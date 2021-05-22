3a13a8e61b55f36a00bbeb486854548720431d3b
Dooole <75305901+Dooole@users.noreply.github.com>
2021-05-22 16:18:32 +0300

    Add device and config controllers and views

---
+++ WrtmWebApp/Controllers/ConfigurationController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WrtmWebApp.Models;
+
+namespace WrtmWebApp.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        private wrtmDBEntities db = new wrtmDBEntities();
+
+        // GET: Configuration
+        public ActionResult Index()
+        {
+            return View(db.Configurations.ToList());
+        }
+
+        // GET: Configuration/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Configuration configuration = db.Configurations.Find(id);
+            if (configuration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(configuration);
+        }
+
+        // GET: Configuration/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Configuration/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Hostname,Ipaddr,Netmask,Gateway,Dns1,Dns2")] Configuration configuration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Configurations.Add(configuration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(configuration);
+        }
+
+        // GET: Configuration/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Configuration configuration = db.Configurations.Find(id);
+            if (configuration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(configuration);
+        }
+
+        // POST: Configuration/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Hostname,Ipaddr,Netmask,Gateway,Dns1,Dns2")] Configuration configuration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(configuration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(configuration);
+        }
+
+        // GET: Configuration/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Configuration configuration = db.Configurations.Find(id);
+            if (configuration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(configuration);
+        }
+
+        // POST: Configuration/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Configuration configuration = db.Configurations.Find(id);
+            db.Configurations.Remove(configuration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
